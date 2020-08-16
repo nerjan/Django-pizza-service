a1ccb1da86479b4fde76b1b0eb25c1bc1c1c5e57
nerjan <41588342+nerjan@users.noreply.github.com>
2020-08-16 22:43:14 +0200

    Add files via upload

---
+++ accounts/admin.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from django.contrib import admin
+from . import models
+from django.forms import Textarea
+from django.db.models.fields import TextField
+#from .models import pizza
+# admin.site.register(pizza)
+
